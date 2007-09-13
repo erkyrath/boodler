887b2db18ec8518866dd961974b0d533041d3a10
Andrew Plotkin <erkyrath@eblong.com>
2007-09-13 04:17:54 +0000

    First draft of distutils packaging for Boodler.
    git-svn-id: https://boodler.svn.sourceforge.net/svnroot/boodler/core/trunk@75 9080d0e4-c72c-0410-a6e4-d27fa395d48a

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+from distutils.command.build_ext import build_ext
+
+class local_build_ext(build_ext):
+	user_options = (build_ext.user_options + [
+		('intmath', None, 'force audio sample computation to use integer math'),
+	])
+	boolean_options = (build_ext.boolean_options + [ 'intmath' ])
+	
+	def initialize_options(self):
+		build_ext.initialize_options(self)
+		self.intmath = None
+
+	def finalize_options(self):
+		if (self.intmath):
+			# Add BOODLER_INTMATH to the compiler macros.
+			if (not self.define):
+				self.define = 'BOODLER_INTMATH'
+			else:
+				self.define = self.define + ',BOODLER_INTMATH'
+		
+		build_ext.finalize_options(self)
+		
+ext_cboodle = Extension('boodle.cboodle',
+	['cboodle/audev-file.c', 'cboodle/cboodle.c', 'cboodle/noteq.c', 'cboodle/sample.c']
+)
+
+setup(name = 'Boodler',
+	version = '1.6.0',
+	description = 'Programmable soundscape tool',
+	author = 'Andrew Plotkin',
+	author_email = 'erkyrath@eblong.com',
+	url = 'http://boodler.org/',
+	packages = ['boodle'],
+	scripts = ['boodler.py', 'boomsg.py'],
+	ext_modules = [ext_cboodle],
+	cmdclass = {'build_ext':local_build_ext},
+)
