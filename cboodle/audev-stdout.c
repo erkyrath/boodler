26a37327c16fc0a253d6ed29d2edffb37b8da8d2
Andrew Plotkin <erkyrath@eblong.com>
2007-09-16 20:27:15 +0000

    Updated the copyright and URL.
    git-svn-id: https://boodler.svn.sourceforge.net/svnroot/boodler/core/trunk@95 9080d0e4-c72c-0410-a6e4-d27fa395d48a

--- cboodle/audev-stdout.c
+++ cboodle/audev-stdout.c
@@ -1,6 +1,6 @@
 /* Boodler: a programmable soundscape tool
-   Copyright 2002 by Andrew Plotkin <erkyrath@eblong.com>
-   <http://www.eblong.com/zarf/boodler/>
+   Copyright 2007 by Andrew Plotkin <erkyrath@eblong.com>
+   <http://eblong.com/zarf/boodler/>
    This program is distributed under the LGPL.
    See the LGPL document, or the above URL, for details.
 */
--- cboodle/audev-vorbis.c
+++ cboodle/audev-vorbis.c
@@ -1,6 +1,6 @@
 /* Boodler: a programmable soundscape tool
    Copyright 2002-7 by Andrew Plotkin <erkyrath@eblong.com>
-   <http://www.eblong.com/zarf/boodler/>
+   <http://eblong.com/zarf/boodler/>
    This program is distributed under the LGPL.
    See the LGPL document, or the above URL, for details.
 
