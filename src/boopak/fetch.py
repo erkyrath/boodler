21cac7fd41b4890d6a6076aeac7f023187f5eb5a
Andrew Plotkin <erkyrath@eblong.com>
2009-01-10 21:57:51 +0000

    Newer copyright dates are 2007
    git-svn-id: https://boodler.svn.sourceforge.net/svnroot/boodler/core/trunk@376 9080d0e4-c72c-0410-a6e4-d27fa395d48a

--- src/boopak/fetch.py
+++ src/boopak/fetch.py
@@ -1,5 +1,5 @@
 # Boodler: a programmable soundscape tool
-# Copyright 2002-9 by Andrew Plotkin <erkyrath@eblong.com>
+# Copyright 2007-9 by Andrew Plotkin <erkyrath@eblong.com>
 #   <http://boodler.org/>
 # This program is distributed under the LGPL.
 # See the LGPL document, or the above URL, for details.
